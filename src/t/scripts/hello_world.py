be6c7db3954b68f0d0345263af8f562e3a8c9234
Dan Palmer <dan@thread.com>
2021-05-03 18:30:37 +0100

    Fix hello-world

--- src/t/scripts/hello_world.py
+++ src/t/scripts/hello_world.py
@@ -5,5 +5,5 @@ from t import cli
 
 @cli.command(help="A greetings module to demonstrate how t works.")
 @click.option("--name", default="world", help="Who to greet")
-def hello_world():
-    print("Hello world!")
+def hello_world(name):
+    print(f"Hello {name}!")
