70aa2f584fddbc4774222eee23a0bde819357d6c
Dan Palmer <dan@thread.com>
2021-05-09 20:35:43 +0100

    Add some types

--- src/t/utils/output.py
+++ src/t/utils/output.py
@@ -1,23 +1,24 @@
 import sys
+from typing import NoReturn, Optional
 
 import click
 
 
-def default(message, exit_with_code=None):
+def default(message: str, exit_with_code: Optional[int] = None) -> None:
     click.echo(message)
     if exit_with_code:
         sys.exit(exit_with_code)
 
 
-def success(message):
+def success(message: str) -> None:
     click.secho(message, fg="green")
 
 
-def error(message, exit_with_code=None):
+def error(message: str, exit_with_code: Optional[int] = None) -> None:
     click.secho(message, fg="red", bold=True)
     if exit_with_code is not None:
         sys.exit(exit_with_code)
 
 
-def fatal(message):
+def fatal(message: str) -> NoReturn:  # type: ignore
     error(message, exit_with_code=1)
