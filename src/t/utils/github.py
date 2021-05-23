fb37896a7117bf2ed50df476fa7dbc99685c14ff
Dan Palmer <dan@thread.com>
2021-05-23 16:01:48 +0100

    Type issues

--- src/t/utils/github.py
+++ src/t/utils/github.py
@@ -19,7 +19,7 @@ def authenticate() -> None:
     auth.open_browser()
     token = auth.wait()
     api = GhApi(token=token)
-    user = api.users.get_authenticated()
+    user = api.users.get_authenticated()  # type: ignore
 
     with update_config() as config:
         config["github"] = {
@@ -34,12 +34,12 @@ def get_authenticated_client() -> GhApi:
 
 def check_authentication() -> bool:
     github = get_config().get("github", {})
-    return github.get("login") and github.get("password")
+    return bool(github.get("login") and github.get("password"))
 
 
 def get_file_contents(owner, repo, path, ref):
     gh = get_authenticated_client()
-    file_data = gh.repos.get_content(owner, repo, path, ref)
+    file_data = gh.repos.get_content(owner, repo, path, ref)  # type: ignore
     if file_data.encoding != "base64":
         raise ValueError(f"Unsupported encoding {file_data.encoding}")
     return base64.b64decode(file_data.content).decode("utf-8")
